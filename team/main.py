03a0141a0c8d9065e500558de45ec87fae5615a3
misterwolf1621 <felix-wol2808@hotmail.de>
2024-01-25 11:38:44 +0100

    runup trim changed

--- team/main.py
+++ team/main.py
@@ -32,8 +32,8 @@ def main():
 
     trimServoRight = 0
     trimServoLeft = -2
-    trimSpeedLeft = 0.1
-    trimSpeedRight = -0.1
+    trimSpeedLeft = -0.1
+    trimSpeedRight = 0.1
     reset = False
 
     for event in ctrl.dev.read_loop(): 
