d149fb7e91b363a0ce278b80da4c317a697fe8ed
misterwolf1621 <felix-wol2808@hotmail.de>
2024-01-25 11:29:35 +0100

    trim change

--- team/main.py
+++ team/main.py
@@ -90,8 +90,8 @@ def main():
             #Umwandlung LT zu PWM Speed
             propSpeed = 500 * (event.value / 1023)
 
-            speedLeft = 1500 - propSpeed * (1 + trimSpeedLeft) 
-            speedRight = 1500 + propSpeed * (1 + trimSpeedRight) + 5
+            speedLeft = 1500 - propSpeed * (1 + trimSpeedLeft)
+            speedRight = 1500 + propSpeed * (1 + trimSpeedRight) + 10
 
             if(speedLeft > 2000):
                 speedLeft = 2000
